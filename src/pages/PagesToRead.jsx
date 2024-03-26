50fe2fd99071128b784cb751c67d43f882098595
jakareya76 <jakareya1306@gmail.com>
2024-03-26 16:09:49 +0600

    create pages to read and show custom chart with books data

--- src/pages/PagesToRead.jsx
+++ src/pages/PagesToRead.jsx
@@ -1,5 +1,43 @@
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
+import { getLocalBooksData } from "../utility/localStorageData";
+
+const bookData = getLocalBooksData();
+
+const data = bookData.map((book) => ({
+  name: book.bookName,
+  pages: book.totalPages,
+}));
+
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${
+    y + height / 3
+  }
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+    x + width
+  }, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
 const PagesToRead = () => {
-  return <div>PagesToRead</div>;
+  return (
+    <div className="p-20 bg-base-200 rounded-xl">
+      <BarChart width={600} height={400} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="pages" fill="#8884d8" shape={<TriangleBar />} />
+      </BarChart>
+    </div>
+  );
 };
 
 export default PagesToRead;
